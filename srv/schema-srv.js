f16755f68c5fa2c3f94a1a6058f44eca3ff0c163
Amesty <p.amesty.guanipa@accenture.com>
2022-09-28 09:43:03 -0300

    xcrsf token

--- srv/schema-srv.js
+++ srv/schema-srv.js
@@ -1,16 +1,26 @@
 const cds = require('@sap/cds');
-cds.env.features.fetch_csrf = true;
+// const { retrieveJwt } = require("@sap-cloud-sdk/core");
+
+// function getJWT(req) {
+//     if (typeof req._ !== "undefined") {
+//         return retrieveJwt(req._.req);
+//     } else {
+//         return "";
+//     }
+// }
 
 module.exports = async (srv) => {
     const { A_BusinessPartner, A_AddressEmailAddress } = srv.entities;
+    cds.env.features.fetch_csrf = true;
+    
     // Using CDS API      
     const ZAPI_BUSINESS_PARTNER = await cds.connect.to("ZAPI_BUSINESS_PARTNER");
 
     // ********************* A_BusinessPartner ********************* //
     srv.on(['READ', 'CREATE', 'UPDATE'], A_BusinessPartner, req => ZAPI_BUSINESS_PARTNER.run(req.query));
-    // ********************* A_BusinessPartner ********************* //
 
     // ********************* A_AddressEmailAddress ********************* //
     srv.on('*', A_AddressEmailAddress, req => ZAPI_BUSINESS_PARTNER.run(req.query));
-    // ********************* A_AddressEmailAddress ********************* //
 }
+
+cds.env.features.fetch_csrf = false;
